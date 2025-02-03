ed1dd2b72673892bbf81d7ed352ea7320a0e2a6c
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-03 16:43:01 -0300

    feat: simplifica o módulo blog removendo controladores não utilizados e adicionando serviço DynamoDb

--- src/controller/blog.module.ts
+++ src/controller/blog.module.ts
@@ -1,34 +1,10 @@
 import { Module } from '@nestjs/common';
-
-// Importando apenas os controladores e serviços de posts
-import { PostsController } from './blog/posts/posts.controller';
-import { PostsService } from './blog/posts/posts.service';
-
-// Outros controladores e serviços foram comentados
-/*
-import { AuthorsController } from './blog/authors/authors.controller';
-import { AuthorsService } from './blog/authors/authors.service';
-
-import { CategoriesController } from './blog/categories/categories.controller';
-import { CategoriesService } from './blog/categories/categories.service';
-
-import { CommentsController } from './blog/comments/comments.controller';
-import { CommentsService } from './blog/comments/comments.service';
-
-import { ExternalIntegrationsController } from './blog/external-integrations/external-integrations.controller';
-import { ExternalIntegrationsService } from './blog/external-integrations/external-integrations.service';
-*/
+import { PostsController } from '@src/controller/blog/posts/posts.controller';
+import { PostsService } from '@src/controller/blog/posts/posts.service';
+import { DynamoDbService } from '@src/services/dynamoDb';  // Verifique a importação correta
 
 @Module({
-    controllers: [
-        PostsController, // Apenas PostsController será usado
-    ],
-    providers: [
-        PostsService, // Apenas PostsService será usado
-    ],
-    // Exports também apenas para PostsService, caso seja necessário em outros módulos
-    exports: [
-        PostsService,
-    ],
+    controllers: [PostsController],
+    providers: [PostsService, DynamoDbService],
 })
 export class BlogModule { }
