e2b0a064972fc9b34f8cee4f9d1e9e74bb700645
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-14 18:12:34 -0300

    refactor: ajusta importações para usar forwardRef em módulos de subcategorias, categorias, comentários e posts

--- src/modules/blog/authors/authors.module.ts
+++ src/modules/blog/authors/authors.module.ts
@@ -1,6 +1,6 @@
 // src/modules/blog/authors/authors.module.ts
 
-import { Module } from '@nestjs/common'; // Importa o decorator Module para definir o módulo.
+import { Module, forwardRef } from '@nestjs/common'; // Importa o decorator Module para definir o módulo.
 import { AuthorsController } from '@src/modules/blog/authors/controllers/authors.controller'; // Importa AuthorsController usando @src.
 import { AuthorsService } from '@src/modules/blog/authors/services/authors.service'; // Importa AuthorsService usando @src.
 import { BlogModule } from '@src/modules/blog.module'; // <--- IMPORTA BlogModule AQUI!
--- src/modules/blog/posts/posts.module.ts
+++ src/modules/blog/posts/posts.module.ts
@@ -1,6 +1,6 @@
 // src/modules/blog/posts/posts.module.ts
 
-import { Module } from '@nestjs/common'; // Importa o decorator Module do NestJS.
+import { Module, forwardRef } from '@nestjs/common'; // Importa o decorator Module para definir o módulo.
 import { PostsController } from '@src/modules/blog/posts/controllers/posts.controller'; // Importa PostsController usando alias @src.
 import { PostsService    } from '@src/modules/blog/posts/services/posts.service'; // Importa PostsService usando alias @src.
 import { BlogModule } from '@src/modules/blog.module'; // <--- IMPORTA BlogModule AQUI!
