8846f03d4a2280cf1bce3d158cb64ca94828c6ad
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-14 18:08:58 -0300

    refactor: ajusta importações do BlogModule em módulos de subcategorias, categorias, comentários e posts para resolver dependências circulares

--- src/modules/blog/authors/authors.module.ts
+++ src/modules/blog/authors/authors.module.ts
@@ -3,12 +3,14 @@
 import { Module } from '@nestjs/common'; // Importa o decorator Module para definir o módulo.
 import { AuthorsController } from '@src/modules/blog/authors/controllers/authors.controller'; // Importa AuthorsController usando @src.
 import { AuthorsService } from '@src/modules/blog/authors/services/authors.service'; // Importa AuthorsService usando @src.
-import { BlogModule } from '@src/modules/blog/blog.module'; // <--- IMPORTA BlogModule AQUI!
+import { BlogModule } from '@src/modules/blog.module'; // <--- IMPORTA BlogModule AQUI!
 
 @Module({
-    imports: [BlogModule], // <--- ADICIONE BlogModule AO ARRAY imports!
+    imports: [forwardRef(() => BlogModule)], // Use forwardRef envolvendo BlogModule para resolver dependência circular
     controllers: [AuthorsController],
     providers: [AuthorsService],
     exports: [AuthorsService],
 })
 export class AuthorsModule { }
+
+
