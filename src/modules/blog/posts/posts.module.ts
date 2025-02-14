0bc7efff0a06c2de4c590a8f497fc29ea10f1a4b
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-14 16:20:22 -0300

    refactor: ajusta importações e scripts de inicialização para melhorar a clareza e a estrutura do projeto

--- src/modules/blog/posts/posts.module.ts
+++ src/modules/blog/posts/posts.module.ts
@@ -2,7 +2,7 @@
 
 import { Module } from '@nestjs/common'; // Importa o decorator Module do NestJS.
 import { PostsController } from '@src/modules/blog/posts/controllers/posts.controller'; // Importa PostsController usando alias @src.
-import { PostsService } from '@src/modules/blog/posts/services/posts.service'; // Importa PostsService usando alias @src.
+import { PostsService    } from '@src/modules/blog/posts/services/posts.service'; // Importa PostsService usando alias @src.
 
 @Module({
     controllers: [PostsController],
