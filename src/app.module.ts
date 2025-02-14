633bc31f8713ad11ae63a7d70586a637c8640946
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-13 23:37:22 -0300

    refactor: atualiza módulo principal para incluir AppController, AppService e DynamoDbService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
-import { BlogModule } from './modules/blog/blog.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DynamoDbService } from './services/dynamoDb.service'; // Importe o DynamoDbService
+import { BlogModule } from './modules/blog/blog.module'; // Importe o BlogModule
 
 @Module({
-  imports: [
-    BlogModule, // Importa o módulo Blog
-  ],
-  controllers: [],
-  providers: [],
+  imports: [BlogModule], // Importe o BlogModule aqui
+  controllers: [AppController],
+  providers: [AppService, DynamoDbService], // Declare o DynamoDbService como provider global
 })
 export class AppModule { }
