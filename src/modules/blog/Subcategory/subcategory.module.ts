070e7ef78884126fb727ca8b3e6ba84943510e73
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-14 17:51:42 -0300

    refactor: adiciona importação do BlogModule em módulos de subcategorias, categorias, comentários e posts para melhorar a estrutura do projeto

--- src/modules/blog/Subcategory/subcategory.module.ts
+++ src/modules/blog/Subcategory/subcategory.module.ts
@@ -3,6 +3,7 @@
 import { Module } from '@nestjs/common';
 import { SubcategoryController } from '@src/modules/blog/subcategory/controllers/subcategory.controller';
 import { SubcategoryService } from '@src/modules/blog/subcategory/services/subcategory.service';
+import { BlogModule } from '@src/modules/blog/blog.module'; // <--- IMPORTA BlogModule AQUI!
 
 @Module({
     controllers: [SubcategoryController],
--- src/modules/blog/category/category.module.ts
+++ src/modules/blog/category/category.module.ts
@@ -3,6 +3,7 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from '@src/modules/blog/category/controllers/category.controller';
 import { CategoryService } from '@src/modules/blog/category/services/category.service';
+import { BlogModule } from '@src/modules/blog/blog.module'; // <--- IMPORTA BlogModule AQUI!
 
 @Module({
     controllers: [CategoryController],
