e707ce6bbe2cf58f8f69ae2cfe771541d1e47851
RainerTeixeira <raineroliveira94@hotmail.com>
2025-02-15 14:59:36 -0300

    refactor: ajusta CommentDto e melhora a documentação no AuthorsController

--- src/modules/blog/comments/dto/comment.dto.ts
+++ src/modules/blog/comments/dto/comment.dto.ts
@@ -1,8 +1,8 @@
 // src/modules/blog/comments/dto/comment.dto.ts
 
 export class CommentDto {
-    postId: number; // Chave de Partição (postId) - Number
-    authorId: string; // Chave de Classificação (authorId) - String
+    postId: string; // Chave de Partição (postId) - Number
+    commentId: string; // Chave de Classificação (commentId ) - String
     content: string;
     date: string; // Formato de data ISO String (ex: "2025-01-12T00:00:00Z")
     status: string; // Ex: "published", "pending", etc.
