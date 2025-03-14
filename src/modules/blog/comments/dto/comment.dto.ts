4b402daebcfb9fb20aa05b246c71daac2eeb3df9
RainerTeixeira <raineroliveira94@hotmail.com>
2025-03-13 23:35:44 -0300

    feat: adiciona documentação Swagger para o módulo de comentários com operações CRUD

--- src/modules/blog/comments/dto/comment.dto.ts
+++ src/modules/blog/comments/dto/comment.dto.ts
@@ -1,8 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 // src/modules/blog/comments/dto/comment.dto.ts
 export class CommentDto {
+    @ApiProperty({ description: 'ID do post' })
     postId: string;
+
+    @ApiProperty({ description: 'ID do comentário' })
     commentId: string;
+
+    @ApiProperty({ description: 'Conteúdo do comentário' })
     content: string;
+
+    @ApiProperty({ description: 'Data do comentário' })
     date: string;
+
+    @ApiProperty({ description: 'Status do comentário' })
     status: string;
 }
