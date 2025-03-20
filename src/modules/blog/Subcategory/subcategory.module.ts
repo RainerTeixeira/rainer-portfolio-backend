43228a2a65b0739a8c24270ea5043ae482790c43
RainerTeixeira <raineroliveira94@hotmail.com>
2025-03-20 13:46:44 -0300

    refactor: adiciona cache-manager e ajusta imports com forwardRef para resolver dependências circulares

--- src/modules/blog/Subcategory/subcategory.module.ts
+++ src/modules/blog/Subcategory/subcategory.module.ts
@@ -4,7 +4,7 @@ import { Module, forwardRef } from '@nestjs/common';
 import { SubcategoryController } from '@src/modules/blog/subcategory/controllers/subcategory.controller';
 import { SubcategoryService } from '@src/modules/blog/subcategory/services/subcategory.service';
 import { BlogModule } from '@src/modules/blog.module';
-import { DynamoDbService } from '@src/services/dynamodb.service';
+import { DynamoDbService } from '@src/services/dynamoDb.service';
 
 @Module({
   imports: [forwardRef(() => BlogModule)],
--- src/modules/blog/category/category.module.ts
+++ src/modules/blog/category/category.module.ts
@@ -1,15 +1,15 @@
 // src/modules/blog/categories/categories.module.ts
 
-import { Module, forwardRef } from '@nestjs/common'; // Importa o decorator Module para definir o módulo.
+import { Module, forwardRef } from '@nestjs/common';
 import { CategoryController } from '@src/modules/blog/category/controllers/category.controller';
 import { CategoryService } from '@src/modules/blog/category/services/category.service';
-import { BlogModule } from '@src/modules/blog.module'; // <--- IMPORTA BlogModule AQUI!
+import { BlogModule } from '@src/modules/blog.module';
 import { DynamoDbService } from '@src/services/dynamoDb.service';
 
 @Module({
-    imports: [forwardRef(() => BlogModule)], // Use forwardRef envolvendo BlogModule para resolver dependência circular
-    controllers: [CategoryController],
-    providers: [CategoryService, DynamoDbService],
-    exports: [CategoryService],
+  imports: [forwardRef(() => BlogModule)],
+  controllers: [CategoryController],
+  providers: [CategoryService, DynamoDbService],
+  exports: [CategoryService],
 })
-export class CategoryModule { }
+export class CategoryModule {}
