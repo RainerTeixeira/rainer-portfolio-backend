512016d995d855146e13f47641bff076f9b93668
RainerTeixeira <raineroliveira94@hotmail.com>
2025-03-20 11:23:13 -0300

    refactor: atualiza imports e adiciona ConfigModule e CacheModule ao módulo de postagens

--- src/modules/blog/posts/posts.module.ts
+++ src/modules/blog/posts/posts.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { PostsService } from './services/posts.service';
-import { PostsController } from './controllers/posts.controller';
+import { PostsService } from '@src/modules/blog/posts/services/posts.service';
+import { PostsController } from '@src/modules/blog/posts/controllers/posts.controller';
 
 import { AuthorsModule } from '@src/modules/blog/authors/authors.module';
 import { CategoryModule } from '@src/modules/blog/category/category.module';
 import { SubcategoryModule } from '@src/modules/blog/subcategory/subcategory.module';
 import { CommentsModule } from '@src/modules/blog/comments/comments.module';
+
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 /**
  * Módulo para gerenciamento de posts.
@@ -17,6 +20,8 @@ import { CommentsModule } from '@src/modules/blog/comments/comments.module';
     CategoryModule,
     SubcategoryModule,
     CommentsModule,
+    ConfigModule, 
+    CacheModule,
   ],
   controllers: [PostsController],
   providers: [PostsService],
