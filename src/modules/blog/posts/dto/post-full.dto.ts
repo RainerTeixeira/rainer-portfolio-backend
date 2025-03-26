87eb10a7d0bcb5566799ea4c6756b56745afc774
RainerTeixeira <raineroliveira94@hotmail.com>
2025-03-26 10:24:00 -0300

    fix: corrige o caminho de importação do CategoryDto no post-full.dto.ts

--- src/modules/blog/posts/dto/post-full.dto.ts
+++ src/modules/blog/posts/dto/post-full.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { PostContentDto } from '@src/modules/blog/posts/dto/post-content.dto';
 import { AuthorDto } from '@src/modules/blog/authors/dto/Author-detail.dto.ts';
-import { CategoryDto } from '@src/modules/blog/categories/dto/category.dto';
+import { CategoryDto } from '@src/modules/blog/category/dto/category.dto';
 import { SubcategoryDto } from '@src/modules/blog/subcategory/dto/subcategory.dto';
 import { CommentDto } from '@src/modules/blog/comments/dto/comment.dto';
 
