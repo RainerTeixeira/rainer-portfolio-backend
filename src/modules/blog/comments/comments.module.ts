5b3310569bf961fe92c4c2bc743d7655d1c66a9d
RainerTeixeira <raineroliveira94@hotmail.com>
2025-04-16 15:39:43 -0300

    refactor: atualizar módulos de subcategorias, categorias e comentários para integrar o CacheModule, melhorando a gestão de cache e a documentação das responsabilidades

--- src/modules/blog/comments/comments.module.ts
+++ src/modules/blog/comments/comments.module.ts
@@ -1,23 +1,36 @@
 import { Module } from '@nestjs/common';
-import { DynamoDbModule } from '@src/services/dynamoDb.service';
+import { CacheModule } from '@nestjs/cache-manager';
+
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { CommentsRepository } from './comments.repository';
-import { CacheModule } from '@nestjs/cache-manager';
+
+/**
+ * Responsabilidades:
+ * - Disponibiliza endpoints REST para operações de comentários.            
+ * - Gerencia dependências de serviço, repositório e cache.
+ *  - Exporta o serviço de comentários para uso em outros módulos.
+ *
+ * Estrutura:
+ * - Controller: CommentsController
+ * - Service: CommentsService
+ * - Repository: CommentsRepository
+ * - Integração com DynamoDbService (global) e CacheModule
+ *
+ * @module CommentsModule       
+ * 
+ */
 
 @Module({
     imports: [
-        DynamoDbModule,
+        // Cache local (se você quiser uma config diferente da global)
         CacheModule.register({
-            ttl: 300, // 5 minutos
-            max: 100 // Limite de itens em cache
-        })
+            ttl: 300,
+            max: 100,
+        }),
     ],
     controllers: [CommentsController],
-    providers: [
-        CommentsService,
-        CommentsRepository
-    ],
-    exports: [CommentsService]
+    providers: [CommentsService, CommentsRepository],
+    exports: [CommentsService],
 })
 export class CommentsModule { }
