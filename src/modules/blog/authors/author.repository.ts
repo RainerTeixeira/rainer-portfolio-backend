8b26fed281d1eaf5896c0a1d0cf75070ffdbbe29
RainerTeixeira <raineroliveira94@hotmail.com>
2025-04-24 15:28:16 -0300

    refactor: atualizar a entidade Subcategory, melhorando a documentação e a estrutura, além de otimizar métodos no repositório e serviço

--- src/modules/blog/authors/author.repository.ts
+++ src/modules/blog/authors/author.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { AuthorEntity } from './authors.entity';
 import { DynamoDbService } from '../../../services/dynamoDb.service';
+import { AuthorEntity } from './authors.entity';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 
