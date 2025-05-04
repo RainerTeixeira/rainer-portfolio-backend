31bb82daa69860ac83215d50548778206724d209
RainerTeixeira <raineroliveira94@hotmail.com>
2025-05-03 23:12:10 -0300

    refactor: adicionar documentação ao método formatDate para melhor compreensão

--- src/common/utils/format-date.ts
+++ src/common/utils/format-date.ts
@@ -1,3 +1,9 @@
+/**
+ * Formata um objeto Date para uma string no formato 'YYYY-MM-DD HH:mm:ss', sem milissegundos.
+ *
+ * @param date - Objeto Date a ser formatado.
+ * @returns Uma string representando a data e hora no formato 'YYYY-MM-DD HH:mm:ss'.
+ */
 export function formatDate(date: Date): string {
     // Retorna no formato YYYY-MM-DD HH:mm:ss (sem milissegundos)
     const pad = (n: number, z = 2) => String(n).padStart(z, '0');
